7a24edde4785c163fa64a31f26152aae08aeca1f
exe <pintotechno@gmail.com>
2026-02-17 12:16:27 +0330

    build green before cleanup 2

--- src/pages-sections/vendor-dashboard/dashboard/recent-purchase.tsx
+++
@@ -1,3 +0,0 @@
-export default async function RecentPurchase() {
-    return null;
-}
--- src/pages-sections/vendor-dashboard/seller-package/types.ts
+++
@@ -1,7 +0,0 @@
-export interface PackageItem {
-  id: number;
-  icon: string;
-  price: number;
-  features: string[];
-  packageName: string;
-}
